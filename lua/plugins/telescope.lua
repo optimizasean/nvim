5fc4892ea4598230bb8ceab8d93ffb8b9d7e2778
optimizasean <optimizasean@gmail.com>
2025-11-04 23:22:15 -1000

    Feat: add telescope colorscheme hotkey

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -10,10 +10,11 @@ return {
     -- Replaces mru.vim
     { '<leader>f', '<cmd>Telescope oldfiles<cr>', desc = 'Find Recent Files' },
     -- Your existing maps
-    { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
-    { '<leader>fg', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
-    { '<leader>fb', '<cmd>Telescope buffers<cr>', desc = 'Find Buffers' },
-    { '<leader>fh', '<cmd>Telescope help_tags<cr>', desc = 'Find Help Tags' },
+    { '<leader>tf', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
+    { '<leader>tg', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
+    { '<leader>tb', '<cmd>Telescope buffers<cr>', desc = 'Find Buffers' },
+    { '<leader>th', '<cmd>Telescope help_tags<cr>', desc = 'Find Help Tags' },
+    { '<leader>tc', '<cmd>Telescope colorscheme<cr>', desc = 'Find Help Tags' }
   },
   config = function()
     require('telescope').setup()
