1c8367016573871917fc5fd7a964b4c3e440e77c
optimizasean <optimizasean@gmail.com>
2026-01-07 17:08:02 -1000

    Fix: treesitter installation issues - force to master branch

--- lua/plugins/nvim-treesitter.lua
+++ lua/plugins/nvim-treesitter.lua
@@ -5,6 +5,7 @@
 return {
   'nvim-treesitter/nvim-treesitter',
   build = ':TSUpdate',
+  branch = 'master',
   -- Add rainbow-delimiters as a dependency
   dependencies = { 'HiPhish/rainbow-delimiters.nvim' },
   config = function()
