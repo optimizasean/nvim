429b33619f7e92265528f76e69c797f806a1d946
optimizasean <optimizasean@gmail.com>
2026-02-10 14:57:23 -1000

    Feat: integrate TypeScript linting

--- lua/plugins/disabled.instant.lua
+++ lua/plugins/disabled.instant.lua
@@ -6,6 +6,6 @@ return {
   'jbyuki/instant.nvim',
   enabled = false,
   config = function()
-    -- vim.g.instant_username = "optimizasean"
+    -- vim.g.instant_username = 'optimizasean'
   end
 }
--- lua/plugins/nvim-notify.lua
+++ lua/plugins/nvim-notify.lua
@@ -11,8 +11,8 @@ return {
 
     -- 2. Call the setup function on it
     notify.setup({
-      background_colour = "NormalFloat", -- Load from colorscheme (transparent.nvim works on this)
-      -- background_colour = "#000000", -- Default for transparent backgrounds
+      background_colour = 'NormalFloat', -- Load from colorscheme (transparent.nvim works on this)
+      -- background_colour = '#000000', -- Default for transparent backgrounds
     })
 
     -- 3. Explicitly set the global vim.notify
