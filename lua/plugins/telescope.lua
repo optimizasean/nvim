b5c1684d8340f50ab8fbc38763de2e97f40a6d02
optimizasean <optimizasean@gmail.com>
2026-02-10 15:37:51 -1000

    Feat: add telescope lsp lookups

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -11,11 +11,20 @@ return {
   keys = {
     -- Replaces mru.vim
     { '<leader>f', '<cmd>Telescope oldfiles<cr>', desc = 'Find Recent Files' },
-    -- Your existing maps
+
+    -- Existing file pickers
     { '<leader>tf', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
     { '<leader>tg', '<cmd>Telescope live_grep<cr>', desc = 'Live Grep' },
     { '<leader>tb', '<cmd>Telescope buffers<cr>', desc = 'Find Buffers' },
     { '<leader>tt', '<cmd>Telescope help_tags<cr>', desc = 'Find Help Tags' },
+
+    -- LSP Navigation (Added these)
+    { 'gd', '<cmd>Telescope lsp_definitions<cr>', desc = '[G]oto [D]efinition' },
+    { 'gr', '<cmd>Telescope lsp_references<cr>', desc = '[G]oto [R]eferences' },
+    { 'gi', '<cmd>Telescope lsp_implementations<cr>', desc = '[G]oto [I]mplementation' },
+    { '<leader>ds', '<cmd>Telescope lsp_document_symbols<cr>', desc = '[D]ocument [S]ymbols' },
+
+    -- Theme Picker
     -- { '<leader>tc', '<cmd>Telescope colorscheme<cr>', desc = 'Find Help Tags' }
     {
       '<leader>tc',
