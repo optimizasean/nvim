0f0473b3f2c58f826e3221eb2241bbbc8976b9d3
optimizasean <optimizasean@gmail.com>
2026-02-26 15:04:48 -1000

    Feat: install support for vue, tailwindcss, html, and css

--- lua/plugins/typescript-tools.lua
+++ lua/plugins/typescript-tools.lua
@@ -6,11 +6,29 @@
 -- ================================================
 return {
   'pmizio/typescript-tools.nvim',
-  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
+  dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig', 'williamboman/mason.nvim' },
   config = function()
+    local mason_registry = require("mason-registry")
+    local vue_plugin_path = ""
+    
+    -- Try to find the vue-language-server package in Mason
+    local ok, vue_ls = pcall(mason_registry.get_package, "vue-language-server")
+    if ok and vue_ls:is_installed() then
+      vue_plugin_path = vue_ls:get_install_path() .. "/node_modules/@vue/typescript-plugin"
+    end
+
     -- Use typescript-tools setup instead of lspconfig.ts_ls.setup
     require('typescript-tools').setup({
       tsserver_path = "/home/optimizasean/.local/share/pnpm/tsserver",
+      filetypes = {
+        "javascript",
+        "javascriptreact",
+        "javascript.jsx",
+        "typescript",
+        "typescriptreact",
+        "typescript.tsx",
+        "vue",
+      },
       on_attach = function(client, bufnr)
         -- Custom keymaps for TS
         local opts = { buffer = bufnr }
@@ -23,6 +41,13 @@ return {
         separate_diagnostic_server = true,
         -- publish_diagnostic_on = 'insert_leave',
         expose_as_code_action = 'all',
+        tsserver_plugins = {
+          {
+            name = "@vue/typescript-plugin",
+            location = vue_plugin_path,
+            languages = { "vue" },
+          },
+        },
         tsserver_file_config = {
           includeInlayParameterNameHints = 'all',
           includeInlayFunctionParameterTypeHints = true,
